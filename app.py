cee44627a9ee28b2631451652224a69ea6d267be
aya.bentaarit <taarit.aya@gmail.com>
2022-01-24 22:21:33 +0100

    feat : ability to show and download file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def butopea():
-    return "<p>Hello, butopea!</p>"
+    return "Here you can find the XML file shown, and you can downloaded it"
 
 if __name__ == "__main__" :
     app.run() 
